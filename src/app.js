38cb9ec91cf1bf0172852d67d21f01e4b168370f
Your Name <apatel7748@gmail.com>
2026-05-28 14:11:39 +0530

    with cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import express from "express";
 import { genetatedErrors } from './middleware/errors.js';
 import authRoutes from "./routes/auth.js";
@@ -16,10 +15,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true
+// }))
 
 // Middleware
 app.use(cookieParser());
