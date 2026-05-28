312a6c893d0be0cdd03f40ede1e7cfea8452b0b6
Your Name <apatel7748@gmail.com>
2026-05-28 14:46:07 +0530

    database connection

--- src/db/Database.js
+++ src/db/Database.js
@@ -6,12 +6,12 @@ async function connectToDatabase() {
         return;
     }
     try {
-        const connectionOptions = {
-            serverSelectionTimeoutMS: 60000, // Increase timeout to 60s
-            socketTimeoutMS: 45000,          // Close sockets after 45s of inactivity
-            family: 4                        // Force IPv4 if IPv6 is unstable
-        };
-        await mongoose.connect(process.env.MONGODB_URI, connectionOptions);
+        // const connectionOptions = {
+        //     serverSelectionTimeoutMS: 60000, // Increase timeout to 60s
+        //     socketTimeoutMS: 45000,          // Close sockets after 45s of inactivity
+        //     family: 4                        // Force IPv4 if IPv6 is unstable
+        // };
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to the database successfully");
     } catch (error) {
         console.error("MongoDB Connection Error Details:", error.message);
