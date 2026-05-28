d92d168293fa625411120606014dcd05145fd9aa
Your Name <apatel7748@gmail.com>
2026-05-28 14:27:33 +0530

    fix cors

--- src/app.js
+++ src/app.js
@@ -15,10 +15,10 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//     credentials: true
-// }))
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
+}))
 
 // Middleware
 app.use(cookieParser());
